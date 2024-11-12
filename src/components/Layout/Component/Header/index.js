45f18762ea75ca2b5c8bfc77bb1fd602f39db7cb
hung.nguyenhuy10 <hung.nguyenhuy10@mobifone.vn>
2024-11-12 17:01:51 +0700

    12/11/2024 header

--- src/components/Layout/Component/Header/index.js
+++ src/components/Layout/Component/Header/index.js
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Header.module.scss";
 import className from "classnames/bind";
 import images from "../../../../assets/images";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+// import Tippy from "@tippyjs/react";
+import Tippy from "@tippyjs/react/headless";
+import { Wrapper as PopperWrapper } from "../../../Popper";
+import AccountItem from "../../../AccountItem";
+import Button from "../../../Button";
 
 const cx = className.bind(styles);
 export default function Header() {
-  return (
-      <header className={cx('wrapper')}>
-          <div className={cx('inner')}>
-              <img src={images.logo} alt="Tiktok" />
-              <div className={cx('search')}>
-                  <input placeholder="Search accounts and videos" spellCheck={false} />
-                  <button className={cx('clear')}>
-                      <FontAwesomeIcon icon={faCircleXmark} />
-                  </button>
-                  <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+  // const [searchResult, setSearchResult] = useState([]);
 
-                  <button className={cx('search-btn')}>
-                      <FontAwesomeIcon icon={faMagnifyingGlass} />
-                  </button>
-              </div>
-              <div className={cx('actions')}></div>
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setSearchResult([1, 2, 3]);
+  //   }, 0);
+  // });
+  const Loginsssss = () => {
+    console.log("sssss");
+  };
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <img src={images.logo} alt="Tiktok" />
+
+        <Tippy
+          interactive
+          //   visible={searchResult.lenght > 0}
+          render={(attrs) => (
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                {" "}
+                Kết quả
+                <h4 className={cx("search-title")}>Accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input
+              placeholder="Search accounts and videos"
+              spellCheck={false}
+            />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
           </div>
-      </header>
+        </Tippy>
+        <div className={cx("actions")}>
+          <Button onClick={Loginsssss}>Loginsssss</Button>
+        </div>
+      </div>
+    </header>
   );
 }
---
+++ src/components/Popper/Wrapper.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./Popper.module.scss";
+
+const cx = classNames.bind(styles);
+export default function Wrapper(children) {
+  return <div className={cx('wrapper')}>Wrapper</div>;
+}
